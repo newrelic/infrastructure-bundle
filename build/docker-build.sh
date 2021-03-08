08d94226eda4eee3abefe43b3f89e82c09a522e1
Roberto Santalla <roobre@roobre.es>
2021-03-08 10:52:02 +0100

    do not default to latest

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -2,8 +2,10 @@
 
 # Wrapper script for docker buildx build setting default values
 
+# Version of https://hub.docker.com/r/newrelic/infrastructure to use as a base
+AGENT_VERSION=${AGENT_VERSION:-"1.15.2"}
+
 DOCKER_PLATFORMS=${DOCKER_PLATFORMS:-linux/amd64,linux/arm64,linux/arm}
-AGENT_VERSION=${AGENT_VERSION:-latest}
 JRE_VERSION=${JRE_VERSION:-} # Blank will pull default version for alpine image
 DOCKER_IMAGE=${DOCKER_IMAGE:-newrelic/infrastructure-bundle}
 DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG:-dev} # Overwritten by CI from the release tag
