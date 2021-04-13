c08f0121e4a7a83aa042e36200e69edbbe0c69d2
Roberto Santalla <roobre@roobre.es>
2021-04-13 10:49:44 +0200

    remove content-type check

--- downloader.go
+++ downloader.go
@@ -179,16 +179,6 @@ func fetch(i *integration, outdir string) error {
 			return fmt.Errorf("got status %d when fetching %s", response.StatusCode, url)
 		}
 
-		ct := response.Header.Get("content-type")
-		switch ct {
-		case "application/x-tar":
-			fallthrough
-		case "application/octet-stream":
-			break
-		default:
-			return fmt.Errorf("unexpected contenty type '%s' for %s", ct, url)
-		}
-
 		destination := path.Join(outdir, arch)
 		if i.Subpath != "" {
 			destination = path.Join(destination, i.Subpath)
