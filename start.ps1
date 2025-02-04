336df6cb54250339945ae6c4fb0f27b9f135e670
tmnguyen12 <tmnguyen12@gmail.com>
2025-02-04 15:38:56 -0800

    update start1.ps1 to log .\newrelic-infra.exe

--- start.ps1
+++ start.ps1
@@ -1,5 +1,6 @@
 # // Runs installer in a new scope
 & "C:\newrelic\Program Files\New Relic\newrelic-infra\installer.ps1"
+# & "C:\newrelic\Program Files\New Relic\newrelic-infra\installer.ps1"
 
 $service = Start-Service -Name "newrelic-infra" -PassThru
 
@@ -8,53 +9,42 @@ if ($service.Status -ne 'Running') {
     exit 1
 }
 
-# $process = Start-Process -FilePath  "C:\newrelic\Program Files\New Relic\newrelic-infra\newrelic-infra.exe" -PassThru
+$process = Start-Process -FilePath  "C:\Program Files\New Relic\newrelic-infra\newrelic-infra.exe" -PassThru -NoNewWindow -RedirectStandardOutput stdout.log -RedirectStandardError stderr.log
 
-# Start-Sleep -Seconds 5
-
-# if ($process.)
-
-# if ($process.HasExited) {
-#     Write-Error "newrelic-infra.exe process exited unexpectedly"
-#     exit 1
-# }
-
-
-Write-Output "Neew Relic Infrastructure started successfully"
+if ($process.HasExited) {
+    Write-Error "newrelic-infra.exe process exited unexpectedly"
+    exit 1
+}
 
 # net start newrelic-infra
 
 # Start-Process "C:\newrelic\Program Files\New Relic\newrelic-infra\newrelic-infra.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-apache.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-cassandra.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-consul.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-couchbase.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-elasticsearch.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-f5.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-haproxy.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-jmx.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-kafka.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-memcached.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mongodb.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mssql.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mysql.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-nagios.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-nginx.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-postgresql.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-rabbitmq.exe"
-# Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-redis.exe"
-
-
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-apache.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-cassandra.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-consul.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-couchbase.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-elasticsearch.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-f5.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-haproxy.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-jmx.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-kafka.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-memcached.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mongodb.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mssql.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-mysql.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-nagios.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-nginx.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-postgresql.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-rabbitmq.exe"
+Start-Process "C:\newrelic\New Relic\newrelic-infra\newrelic-integrations\bin\nri-redis.exe"
 
 # Start-Process "C:\newrelic\Program Files\New relic\nrjmx\bin\nrjmx.bat"
+Start-Process "C:\newrelic\var\db\newrelic-infra\nri-discovery-kubernetes.exe"
 
-# Start-Process "C:\newrelic\var\db\newrelic-infra\nri-discovery-kubernetes.exe"
+Write-Output "zzNew Relic Infrastructure started successfully"
 
-
-
-
-
-
-
-
-while ($true) { Start-Sleep -Seconds 60}
+while ($true) { 
+    if (Test-Path stdout.log) { Get-Content stdout.log -Wait -Tail 0 }
+    if (Test-path stderr.log) { Get-Content stderr.log -Wait -Tail 0 }
+    Start-Sleep -Seconds 60
+}
